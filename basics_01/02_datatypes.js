54b271a88b030fcaed9a476f403d544576bc14c9
devnathvishal <110326926+devnathvishal@users.noreply.github.com>
2024-02-12 18:42:52 +0000

    datatypes

---
+++ basics_01/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict"  //Treat all Js Code as newer version
+
+ //alert (3 + 3); // We cant use alert here because we are using node Js not browser
+
+ //number
+ //bigInt
+ //String
+ //null : Standalone value to represent empty value
+ //undefined
+ //boolean
+ //Symbol : unique
+
+ 
+
+ //Object   
