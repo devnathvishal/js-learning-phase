e43ca1ab7fe27337bf17c6cfeb8ae7fd34f473a2
devnathvishal <110326926+devnathvishal@users.noreply.github.com>
2024-02-12 18:43:22 +0000

    varaibles

---
+++ basics_01/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 747456;
+let email = "hyuser@google.com";
+var accountPassword = "919180";
+ accountCity = "Bhagalpur";
+// accountId=44
+
+email= "hellouser@google.com"
+accountPassword = "11817"
+accountCity = "Bharatpur"
+ console.log(accountId);
+ console.table([email,accountPassword,accountCity])
