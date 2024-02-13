c7d6b84ef90701d891c7263243e8b62ddde3ce52
devnathvishal <110326926+devnathvishal@users.noreply.github.com>
2024-02-13 19:08:08 +0000

    Conversion of Data Types

---
+++ basics_01/03_ConversionOperation.js
@@ -0,0 +1,31 @@
+let score = "33aa"
+
+// console.log(typeof score);
+
+let Value = Number(score);
+
+// console.log(typeof Value);
+// console.log(Value);
+
+// "33" = 33
+// "33abc" = Nan
+// undefined =  Nan
+//  null = 0
+// true = 1; false = 0 
+
+let BooleanValue = 1;
+let ConvertValue = Boolean(BooleanValue);
+
+// console.log(ConvertValue);
+
+let ValueBoolean = ""
+let ConvertString = Boolean(ValueBoolean);
+// console.log(ConvertString);
+
+let NameString = "abc"
+let ConversionName = Boolean(NameString);
+console.log(ConversionName);
+
+// 0 => false; 1 => true
+// "" => False; "abc" => true
+
