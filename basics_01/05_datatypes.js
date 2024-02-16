76dbd91d0abfc7dcf77d2cf7d56eb3fef85e6cf2
devnathvishal <110326926+devnathvishal@users.noreply.github.com>
2024-02-16 10:09:53 +0000

    datatypes

---
+++ basics_01/05_datatypes.js
@@ -0,0 +1,30 @@
+// Primitive Data Types
+
+//7 Types
+//String, Number, Null, Undefined, Boolean, Symbol, BigInt
+
+const num1 = 21;
+const valString = "Value";
+const valNull = null;
+let valUndefined;
+const valBoolean = true;
+const valSymbol = Symbol('221');
+const valBigint = 22122019991121111n
+
+
+//Reference Types or Non Primitive data Types
+
+//Arrays , Objects, Functions
+
+const arrayValue = ["Ram","Lakshman", "Hanumaan"];
+const objValue = {
+                    name: "Vishal",
+                    age:  26,
+                    City: "Noida"
+                 }
+
+const funValue = function()
+               {
+                    console.log("Hello World");
+               }                 
+           console.log(typeof valSymbol);      
