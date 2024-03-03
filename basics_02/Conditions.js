8471599d264b0d414442538728a16e44d4a4a22a
devnathvishal <110326926+devnathvishal@users.noreply.github.com>
2024-03-03 12:41:47 +0000

    Conditions

---
+++ basics_02/Conditions.js
@@ -0,0 +1,23 @@
+/* 
+< , > ,<=,>=, ,= ,==, ===, !=, &&, ||
+
+Falsy values
+"",false,0,-0,BigInt 0n, null, Nan,undefined
+Truthy Values
+"0"," ", "false", [],{}, function(){}
+
+Nullish Coalescing operator (??): null undefined
+
+let val1 = 5 ?? 10
+here firtst value will be assigned
+let val2 = null ?? 2
+let val3 = undefined ?? 8
+
+here in both variables number values will be assigned.
+
+
+Ternary Operator
+
+Condition ? true: false
+
+*/
